79f63aaab9f2c15a805d5396161bcc37c73cbecd
avelinoandre <andreavelino002@gmail.com>
2025-11-16 00:40:18 -0300

    ajustes menu

--- src/MENU/comojogar.c
+++ src/MENU/comojogar.c
@@ -6,13 +6,13 @@ void DrawComoJogarScreen(void) {
     
     ClearBackground((Color){20, 20, 30, 255});
 
-    DrawText("COMO JOGAR", screenWidth/2 - 250, 120, 80, YELLOW);
+    DrawText("COMO JOGAR", screenWidth/2 - 280, 120, 80, YELLOW);
 
-    DrawText("W, A, S, D - Mover o personagem", screenWidth/2 - 230, 240, 35, LIGHTGRAY);
-    DrawText("X - Atacar", screenWidth/2 - 230, 300, 35, LIGHTGRAY);
-    DrawText("ENTER - Voltar ao menu", screenWidth/2 - 230, 360, 35, LIGHTGRAY);
+    DrawText("W, A, S, D - Mover o personagem", screenWidth/2 - 300, 240, 35, LIGHTGRAY);
+    DrawText("X - Atacar", screenWidth/2 - 300, 300, 35, LIGHTGRAY);
+    DrawText("ENTER - Voltar ao menu", screenWidth/2 - 300, 360, 35, LIGHTGRAY);
 
-    DrawText("Pressione ENTER para voltar",screenWidth/2 - 120, screenHeight - 80, 20, GRAY);
+     DrawText("Pressione ENTER para voltar", screenWidth/2 - 180, screenHeight - 80, 20, GRAY);
 }
 
 int UpdateComoJogarScreen(void) {
--- src/MENU/sobre.c
+++ src/MENU/sobre.c
@@ -6,12 +6,12 @@ void DrawSobreScreen(void) {
 
     ClearBackground((Color){20, 20, 30, 255});
 
-    DrawText("SMASH TOONS", screenWidth/2 - 250, 120, 80, YELLOW);
+    DrawText("SMASH TOONS", screenWidth/2 - 300, 120, 80, YELLOW);
 
-    DrawText("Um jogo Beat 'em Up desenvolvido em C com Raylib.", screenWidth/2 - 290, 240, 35, LIGHTGRAY);
-    DrawText("Desenvolvido por André, Caio, Gabriel e Rodrigo.", screenWidth/2 - 290, 300, 35, LIGHTGRAY);
+    DrawText("Um jogo Beat 'em Up desenvolvido em C com Raylib.", screenWidth/2 - 420, 240, 35, LIGHTGRAY);
+    DrawText("Desenvolvido por André, Caio, Gabriel e Rodrigo.", screenWidth/2 - 420, 300, 35, LIGHTGRAY);
 
-    DrawText("Pressione ENTER para voltar", screenWidth/2 - 120, screenHeight - 80, 20, GRAY);
+    DrawText("Pressione ENTER para voltar", screenWidth/2 - 180, screenHeight - 80, 20, GRAY);
 }
 
 int UpdateSobreScreen(void) {
